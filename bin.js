0cb41448270dacc9ca262e03b86f9e5d619c864b
Rik Hoffbauer <62353999+rikhoffbauer@users.noreply.github.com>
2020-04-04 05:53:46 +0200

    chore: initial (placeholder) commit

---
+++ bin.js
@@ -0,0 +1,4 @@
+#!/usr/env/bin node
+console.warn("create-anything is a placeholder package, it has no implementation yet.");
+process.exit(1);
+
---
+++ index.js
@@ -0,0 +1,2 @@
+console.warn("create-anything is a placeholder package, it has no implementation yet.");
+module.exports = {};
